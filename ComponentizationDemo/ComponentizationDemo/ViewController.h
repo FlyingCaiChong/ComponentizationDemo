423f056113d6dc3ed876ff042c09759e6d0ef8bd
fangh <fangheng@beadwallet.com>
2019-03-25 11:50:57 +0800

    setup

---
+++ ComponentizationDemo/ComponentizationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ComponentizationDemo
+//
+//  Created by iOS_Developer on 2019/3/25.
+//  Copyright © 2019年 iOS_Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
